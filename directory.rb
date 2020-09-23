f951473fcdf992095e0511ef37950e4464e2f66c
Tim <timgillviola@gmail.com>
2020-09-23 11:15:28 +0100

    refactored directory

--- directory.rb
+++ directory.rb
@@ -1,50 +1,63 @@
+@students = []
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the studetns"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def process (selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else 
+    puts "I don't know what you mean, please try again."
+  end
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  
-  students = []
   name = gets.chomp
   
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students."
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students."
     name = gets.chomp
   end
-  students
+  @students
 end
-
-# # first we put all the students into an array
-# students = [
-#   {name: "Dr. Hannibal Lecter", cohort: :november},
-#   {name: "Darth Vader", cohort: :november},
-#   {name: "Nurse Ratched", cohort: :november},
-#   {name: "Michael Corleone", cohort: :november},
-#   {name: "Alex DeLarge", cohort: :november},
-#   {name: "The Wicked Witch of the West", cohort: :november},
-#   {name: "Terminator", cohort: :november},
-#   {name: "Freddy Krueger", cohort: :november},
-#   {name: "The Joker", cohort: :november},
-#   {name: "Joffrey Baratheon", cohort: :november},
-#   {name: "Norman Bates", cohort: :november}
-# ]
 
 def print_header
   puts "The students of Villians Academy"
   puts "----------------------"
 end
 
-def print (names)
-  names.each { |name|
-    puts "#{name[:name]} (#{name[:cohort]} cohort)"
+def print_students_list
+  @students.each { |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   }
 end
 
-def print_footer (names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
